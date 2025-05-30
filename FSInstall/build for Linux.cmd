18d8f0f82e6cb06f045dbba3fb706f871969d1bd
NeoCircuit <154694006+NeoCircuit-Studios@users.noreply.github.com>
2025-05-30 11:14:17 +0200

    Remove all files bc of a big error in the scripts

--- FSInstall/build for Linux.cmd
+++
@@ -1,5 +0,0 @@
-@echo  off 
-
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
-
-::dotnet new console -n Myapp
--- OLD/fs-winehelper/NeoCircuit-Studios/Hello_Wine/src/Hello_Wine/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello Wine!, from NeoCircuit-Studios!!");
-    }
-}
